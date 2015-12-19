75decdd0672a6b8dfe46c648cbbd4f3d0f049348
mrmcc3 <mrmcc3@gmail.com>
2015-12-19 22:57:46 +1000

    add url to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject mrmcc3/libsass-clj "0.1.0-SNAPSHOT"
   :description "clojure wrapper for java libsass compiler (JNA)"
+  :url "https://github.com/mrmcc3/libsass-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories [["clojars" {:sign-releases false}]]
