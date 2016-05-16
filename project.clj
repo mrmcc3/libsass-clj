6082d0fc2566e9554437833362f6f6b130c58f56
Michael McClintock <mrmcc3@gmail.com>
2016-05-16 21:07:38 +1000

    release 0.1.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mrmcc3/libsass-clj "0.2.0-SNAPSHOT"
+(defproject mrmcc3/libsass-clj "0.1.5"
   :description "clojure wrapper for libsass (via jsass)"
   :url "https://github.com/mrmcc3/libsass-clj"
   :license {:name "Eclipse Public License"
@@ -6,5 +6,4 @@
   :repositories [["clojars" {:sign-releases false}]]
   :jar-exclusions [#".DS_Store"]
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [io.bit3/jsass "5.1.1"]
-                 [org.slf4j/slf4j-nop "1.7.12" :scope "test"]])
+                 [io.bit3/jsass "5.1.1"]])
